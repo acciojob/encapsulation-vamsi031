c3cffd30e0d1701a7073b32e5835f7bcd565e82b
vamsi <vamsi003311@gmail.com>
2023-06-13 00:15:03 +0530

    add main and RW class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+//        obj.name = "temp";
+        obj.setName("temp2");
+        obj.getName();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
